b6c2aa42a7103c9d5661652191f2df1bab2f70eb
Bagus Setiadi <bsetiadi2605@gmail.com>
2023-11-13 13:35:02 +0700

    use dynamic

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
-import App from '../agora/App'
+import dynamic from 'next/dynamic'
+ 
+const DynamicHeader = dynamic(() => import('../agora/App'), {
+  loading: () => <p>Loading...</p>,
+})
 
 export default function Home() {
   return (
     <div>
-      <App />
+      <DynamicHeader />
     </div>
   )
 }
